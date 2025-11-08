7bb289da99439d23936518f7a98a60c142db7500
= <=>
2025-11-08 16:42:12 -0500

    refactor: migrate repositories and exceptions to Kotlin

--- src/main/kotlin/com/wezaam/withdrawal/repository/WithdrawalEventOutboxRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.wezaam.withdrawal.repository
-
-import com.wezaam.withdrawal.model.WithdrawalEventOutbox
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface WithdrawalEventOutboxRepository : JpaRepository<WithdrawalEventOutbox, Long> {
-    fun findByProcessedAtIsNullOrderByCreatedAtAsc(): List<WithdrawalEventOutbox>
-}
-
