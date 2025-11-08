4d15b17faff965743b16797665abc0d2d220cb4a
= <=>
2025-11-08 16:37:17 -0500

    refactor: migrate controllers and config to Kotlin

---
+++ src/main/kotlin/com/wezaam/withdrawal/Application.kt
@@ -0,0 +1,14 @@
+package com.wezaam.withdrawal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
+
---
+++ src/main/kotlin/com/wezaam/withdrawal/rest/UserController.kt
@@ -0,0 +1,27 @@
+package com.wezaam.withdrawal.rest
+
+import com.wezaam.withdrawal.model.User
+import com.wezaam.withdrawal.repository.UserRepository
+import io.swagger.annotations.Api
+import org.springframework.context.ApplicationContext
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@Api
+@RestController
+class UserController(
+    private val context: ApplicationContext
+) {
+
+    @GetMapping("/find-all-users")
+    fun findAll(): List<User> {
+        return context.getBean(UserRepository::class.java).findAll()
+    }
+
+    @GetMapping("/find-user-by-id/{id}")
+    fun findById(@PathVariable id: Long): User {
+        return context.getBean(UserRepository::class.java).findById(id).orElseThrow()
+    }
+}
+
